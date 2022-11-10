566186219fb2b8591b4ead1c4a97ad6067b06ef1
Jesse Hoogland <jessequinten@gmail.com>
2022-11-10 12:09:58 +0100

    ADD social image link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://agi-curriculum.vercel.app",
   integrations: [tailwind()],
   vite: {
     ssr: {
