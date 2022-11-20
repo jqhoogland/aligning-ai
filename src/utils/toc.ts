53edb885a4376694de6e85d314f65477af5da8fc
Jesse Hoogland <jessequinten@gmail.com>
2022-11-20 18:44:57 +0100

    FIX headings

--- src/utils/toc.ts
+++ src/utils/toc.ts
@@ -1,5 +1,10 @@
-export interface Heading {
-    value: string;
-    level: number
-}
+import { MarkdownHeading, MDXInstance } from "astro";
 
+export function getHeadings(page: MDXInstance<Record<string, any>>): MarkdownHeading[] {
+    return page.getHeadings()
+                .filter((heading) => heading.depth === 1 && heading.text.match(/^\d+\.\d+ .+/))
+                .map((heading) => ({
+                        ...heading,
+                        text: heading.text.replace(/^\d+\.\d+\s+/, ""),
+                }))
+}
